7e4cda61f67fff1176a8a5641a0e3b9396b6530b
Arinelson Santos <48639869+arinelson@users.noreply.github.com>
2025-04-19 00:44:56 -0300

    Update FaqTab.tsx

--- src/components/briefing/FaqTab.tsx
+++ src/components/briefing/FaqTab.tsx
@@ -56,11 +56,19 @@ const FaqTab: React.FC = () => {
       question: "Caso o meu link pare de funcionar durante a campanha, o que devo fazer?",
       answer: (
         <>
-          Caso seja uma decisão rápida, você pode usar um gerador de mensagem personalizada para o WhatsApp.{' '}
+          Caso seja uma decisão rápida, você pode usar um gerador de mensagem personalizada para o WhatsApp {' '}
           <a href="https://arinelson.space/links" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">
-            CLIQUE AQUI
+            (clique aqui). 
           </a>
-          .
+          E dentro desse gerador você insiraria o número 558293460460 e criaria uma mensagem personalizada informando seu CUPOM (como mostra na imagem abaixo).
+          {/* Inserindo a imagem */}
+      <div className="mt-4 flex justify-center">
+        <img 
+          src="/preview.png" 
+          alt="Exemplo de como criar mensagem personalizada" 
+          className="max-w-full h-auto rounded-lg border border-delta-neon/20 shadow-md"
+        />
+      </div>
         </>
       ),
     },
